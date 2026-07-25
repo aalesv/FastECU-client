74d7b166bbe98d9f108cc66402a6a920fc8ceb3c
aalesv <aalesv70@hotmail.com>
2026-07-25 17:21:45 +0300

    Revert "Implemented logging from client to server"
    This reverts commit 90fb2c073a30209d0990de065f4726501d816e01.

--- serial_port/serial_port_actions.h
+++ serial_port/serial_port_actions.h
@@ -143,10 +143,6 @@ public:
 
     unsigned long read_vbatt(void) override;
 
-signals:
-    void LOG_TO_SERVER(QString message, bool timestamp, bool linefeed);
-    void SET_PROGRESSBAR_BY_CLIENT(int value);
-
 private:
     SerialPortActionsDirect        *serial_direct;
 
